171efda67d056ff90e648459bf936d01be4e7ac0
Evan <spanglem@uc.edu>
2025-02-16 19:12:55 -0500

    task 2 final commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.print("Enter your birth month (1-12): ");
-        int month = scanner.nextInt();
+        int month = in.nextInt();
         if (month >= 1 && month <= 12) {
             System.out.println("Your birth month is: " + month);
         } else {
